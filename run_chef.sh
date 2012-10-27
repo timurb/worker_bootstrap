d6f8076033718399798c647da2a480566dcce026
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-10-27 22:08:24 +0400

    retrieve node.json from URL from instance params prior to running chef

--- run_chef.sh
+++ run_chef.sh
@@ -3,5 +3,18 @@
 WORKDIR="$( dirname "$( readlink -f $0 )" )"
 
 CONFIG="$WORKDIR/solo.rb"
+USERDATAFILE="$WORKDIR/userdata.json"
+NODEJSON="$WORKDIR/node.json"
+
+# generate the chef config from the template
 [ -f "$CONFIG" ] || sed "s,@COOKBOOK_PATH@,$WORKDIR," "$CONFIG.template" > "$CONFIG"
-chef-solo -j "$WORKDIR/node.json" -c "$CONFIG" $*
+
+# retrieve node.json from URL from userdata
+JSON_URL="$(curl -s http://169.254.169.254/latest/user-data)"
+[ -n "$JSON_URL" ] && curl -s "$JSON_URL" > "$USERDATAFILE"
+
+# if there is a correct JSON replace default node.json with that
+[ -x "$WORKDIR/validate_json" ] && "$WORKDIR/validate_json" "$USERDATAFILE" && mv -f "$USERDATAFILE" "$NODEJSON"
+
+
+chef-solo -j "$NODEJSON" -c "$CONFIG" $*
